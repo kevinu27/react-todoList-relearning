4ed33388a43993da91800379c892bc14faccac22
unknown <kevin.sm89@gmail.com>
2024-08-06 21:51:45 +0200

    eliminar tarea

--- prueba/src/components/MainToDo.jsx
+++ prueba/src/components/MainToDo.jsx
@@ -5,10 +5,25 @@ import ToDo from './ToDo';
 
 
 export default function MainToDo(props) {
-    const [toDoList, settoDoList] = useState([])
+    const [toDoList, settoDoList] = useState([])//inicio el estado como una array en vez de dejarlo solo porque si no da problemas con el map()
 
     function clickHandler(e) {
         console.log(e)
+        console.log('clickhandler')
+    }
+    function clickHandlerDelete(e, props ) {
+        console.log(e)
+        console.log('---', props.index)
+        console.log('---', props.tarea.task)
+        console.log('delete')
+        // primero al settoDoList le pasa el prevList que react lo detecta como el estado que ya tiene y se lo pasa como arrow funtion
+        //para que se ejecute la funcion de la arrow function si no me equivoco, despues dentro del filter:
+        //el item es el elemento que itera y como en map el segundo elemento que se le pasa lo detecta como numero que de iteraciones que lleva
+        // en este caso entonces mejor que no se modifique el orden del array del estado toDoList
+        //si se cambiase el orden entonces habria que ponerle el id a mano
+        settoDoList(prevList => prevList.filter((item, i) => i !== props.index));
+
+
     }
 
 const descriptcion = "lo que sea"
@@ -38,7 +53,9 @@ return (
     <section className='listHolder'>
     {toDoList.map((tarea, index) => (
 
-<ToDo key={index} tarea={tarea} clicker={clickHandler}></ToDo> //aqui el chiclk handler lopaso como props con la prop clicker
+<ToDo key={index} index={index} tarea={tarea} clicker={clickHandler} delete= {clickHandlerDelete}></ToDo> //aqui el chiclk handler lopaso como props con la prop clickerel 
+//index={index} ques estoy pasando aqui y al que se le asigna la posicion del array que esta itetando lo esta generando el map(), que cuando le pasas un segundo argumento a un map
+//te devuelve la posicion que está iterando
 
 ))}
     </section>
--- prueba/src/components/ToDo.jsx
+++ prueba/src/components/ToDo.jsx
@@ -5,10 +5,16 @@ import './ToDo.css'
 
 export default function ToDo(props) {
 
+    const handleDeleteClick = (e) => { // lo hago asi diferente a la funcion clicker para poder añadirle el stop.propagation y que no me ejecute las dos funciones
+        e.stopPropagation(); // 
+        props.delete( e, props);
+        
+    };
 
 return (
     //aqui la funcion onClick va con una arrow function porque la funcion tiene un argumento
-    <section className='taskCard'onClick={(e) => props.clicker(e)}> 
+    <section className='taskCard' onClick={(e) => props.clicker(e)}> 
+    <p onClick={handleDeleteClick} >X</p>
  <p> {props.tarea.task}</p> 
  <p>  {props.tarea.description}</p> 
     </section>
